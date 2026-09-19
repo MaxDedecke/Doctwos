3ba269b7d98eb3390a117f741d1d5c425bdafbf7
Max Dedecke <jm.dedecke@gmail.com>
2026-09-19 18:54:53 +0000

    fix(shell): reindex after function identity change

--- parser/core/registry.py
+++ parser/core/registry.py
@@ -100,5 +100,5 @@ STRUCTURE_PARSERS: dict[str, ParserEntry] = {
     ),
     "html": ParserEntry(parse=parse_jsp_or_html, root_entity_types=("html_document",), parser_version="html-structure-1"),
     "jsp": ParserEntry(parse=parse_jsp_or_html, root_entity_types=("jsp_page",), parser_version="jsp-structure-1"),
-    "shell": ParserEntry(parse=parse_shell_file, root_entity_types=("shell_script",), parser_version="shell-structure-1"),
+    "shell": ParserEntry(parse=parse_shell_file, root_entity_types=("shell_script",), parser_version="shell-structure-2"),
 }
